79acc4ef142814ba3e1969a6cc79ef74d7580e2e
ahmed.202081 <ahmedmgomaa01@gmail.com>
2023-08-15 23:29:06 +0000

    h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* File: 0-putchar.c
+* Author: [Your Name]
+* Description: This program prints "_putchar" followed by a new line.
+* Return: 0 (success)
+*/
+	int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
+
+	int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
